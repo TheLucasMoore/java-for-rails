648e01f5ec0cfa1070dd83c731042c00ae3e6ad8
TheLucasMoore <lucasedwardmoore@gmail.com>
2018-01-20 22:40:05 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+var public = __dirname + "/app/public/";
+var media = __dirname + "/app/media/";
+
+
+// viewed at http://localhost:8080
+app.get('/', function(req, res) {
+  res.sendFile(path.join(public + "index.html"));
+});
+
+app.use('/media', express.static(media));
+
+app.listen(process.env.PORT || 8080);
