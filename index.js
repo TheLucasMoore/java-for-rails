3b94c977ce749c900eba5dce379c8fba6833c34a
TheLucasMoore <lucasedwardmoore@gmail.com>
2018-01-24 17:52:01 -0700

    https redirect

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+var sslRedirect = require('heroku-ssl-redirect');
 var express = require('express');
 var app = express();
 var path = require('path');
 var public = __dirname + "/app/public/";
 var media = __dirname + "/app/media/";
 
+app.use(sslRedirect());
 
 // viewed at http://localhost:8080
 app.get('/', function(req, res) {
